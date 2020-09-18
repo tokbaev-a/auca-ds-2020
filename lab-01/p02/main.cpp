41e3fe1e9b974b05a44bfff3fb620efcd3da6fd9
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-09-18 10:53:33 +0600

    lab-01 p02 Done

---
+++ lab-01/p02/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+const int kNameWidth = 25;
+const int kAverageWidth = 40;
+
+int main()
+{
+    cout << setfill('.') << fixed << setprecision(2);
+    for (string name; cin >> name;)
+    {
+        double sum = 0;
+        int nGrades = 0;
+        for (int grade; cin >> grade;)
+        {
+            sum += grade;
+            ++nGrades;
+        }
+        if (nGrades != 0)
+        {
+            cout << left << setw(kNameWidth) << name << " | " << right << setw(kAverageWidth) << sum / nGrades << "\n";
+        }
+        else
+        {
+            cout << left << setw(kNameWidth) << name << " | " << right << setw(kAverageWidth) << "no data" << "\n";
+        }
+
+        if (cin.fail())
+        {
+            cin.clear();
+        }
+    }
+}
