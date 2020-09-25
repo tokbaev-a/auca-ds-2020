c646e5e25c12a86fe580eb69b391e012f8a37e85
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-09-25 06:51:25 +0600

    lab-02 p04 The Last Problem Wrong Answer test 6

---
+++ lab-02/p04/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+    cout << "Thank you, " << name << ", and farewell!";
+}
