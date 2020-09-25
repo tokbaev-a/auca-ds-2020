75917e116912e49d14bb90d54a3eceafd08e5bea
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-09-25 09:49:54 +0600

    lab-02 p08 IsItHalloween.com Accepted

--- lab-02/p08/main.cpp
+++ lab-02/p08/main.cpp
@@ -7,5 +7,16 @@ using namespace std;
 
 int main()
 {
+ string month;
+ int date;
+ cin >> month >> date;
+ if((month == "OCT" && date == 31) || (month == "DEC" && date == 25))
+ {
+     cout << "yup";
+ }
+ else
+ {
+     cout << "nope";
+ }
  
 }
