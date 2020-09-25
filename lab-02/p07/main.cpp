fef7d39ba9dd051adb092aa1754ef014d2b3b728
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-09-25 09:35:35 +0600

    lab-02 p07 Tarifa Wrong Answer case 1

---
+++ lab-02/p07/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int n;
+    int res;
+    cin >> x >> n;
+    while (n-- > 0)
+    {
+        int p;
+        cin >> p;
+        res += x - p;
+    }
+    cout << res + x;
+}
---
+++ lab-02/p08/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+ 
+}
