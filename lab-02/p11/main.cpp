74d779c9afb389ab27173084ab960a803a546cc0
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-09-25 11:24:12 +0600

    lab-02 p11 One Chicken Per Person! Accepted

--- lab-02/p11/main.cpp
+++ lab-02/p11/main.cpp
@@ -10,11 +10,20 @@ int main()
 {
     int guests, chiken;
     cin >> guests >> chiken;
-    if(chiken >= guests)
-        cout << "Dr. Chaz will have "<< (chiken-guests) <<" pieces of chicken left over!";
-    else
-        {
-            cout << "Dr. Chaz needs "<< (guests-chiken) <<" more pieces of chicken!";
-        }
-        
+    if(chiken > guests && chiken-guests==1)
+    {
+        cout << "Dr. Chaz will have " << (chiken - guests) << " piece of chicken left over!";
+    }
+    else if (chiken > guests)
+    {
+        cout << "Dr. Chaz will have " << (chiken - guests) << " pieces of chicken left over!";
+    }
+    else if (chiken < guests && guests-chiken==1)
+    {
+        cout << "Dr. Chaz needs "<< (guests-chiken) <<" more piece of chicken!";
+    }
+    else if (chiken < guests)
+    {
+        cout << "Dr. Chaz needs " << (guests - chiken) << " more pieces of chicken!";
+    }
 }
