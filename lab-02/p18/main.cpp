7c2ef7b695b212ecea1082f0b1ba3bbc564b6247
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-10-02 14:03:26 +0600

    lab-02 p18 accepted

--- lab-02/p18/main.cpp
+++ lab-02/p18/main.cpp
@@ -8,9 +8,18 @@ using namespace std;
 
 int main()
 {
-    string name;
-    int pnt;
-
-    cin >> name >> pnt;
-    cout << name << " " << pnt;                       
+    char a, b;
+    int A = 0;
+    int B = 0;
+    while (cin >> a >> b)
+    {
+        if (a == 'A')
+            A += b - '0';
+        else if (a == 'B')
+            B += b - '0';
+    }
+    if (max(A, B) == A)
+        cout << "A\n";
+    else
+        cout << "B\n";
 }
