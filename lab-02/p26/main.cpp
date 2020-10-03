1f6efbffda2533621732c866653fe1dc2595afa3
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-10-04 01:17:35 +0600

    lab-02 p26 accepted

--- lab-02/p26/main.cpp
+++ lab-02/p26/main.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 int main()
 {
-    int n, p, r, f;
+    int n;
+    long long p, r, f;
     cin >> n;
     while (n-- > 0)
     {
---
+++ lab-02/p27/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    
+}
