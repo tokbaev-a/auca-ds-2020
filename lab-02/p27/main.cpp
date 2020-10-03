5e375831b71dc88b20d3264e75a033f9e3409117
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-10-04 02:13:38 +0600

    lab-02 p27 accepted

--- lab-02/p27/main.cpp
+++ lab-02/p27/main.cpp
@@ -8,5 +8,20 @@ using namespace std;
 
 int main()
 {
-    
+    int n, turt1, turt2, ans;
+    turt2 = 1;
+    ans = 0;
+    cin >> n;
+    while (n-- > 0)
+    {
+        while(true)
+        {
+            cin >> turt1;
+            if(turt1 == 0)break;
+            if(turt1>turt2*2)ans+=turt1-(turt2*2);
+            turt2 = turt1;
+        }
+        cout << ans << "\n";
+        ans = 0;
+    }
 }
