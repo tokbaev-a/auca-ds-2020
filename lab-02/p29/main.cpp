717f80004472180a2dc56d6b5478739f644f20e1
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-10-04 15:20:30 +0600

    lab-02 p29 accepted

--- lab-02/p29/main.cpp
+++ lab-02/p29/main.cpp
@@ -11,15 +11,22 @@ int main()
     int a, b, c, d, deg;
     while (true)
     {
-        deg = 1080;
+        deg = 120;
         cin >> a >> b >> c >> d;
         if (a == 0 && b == 0 && c == 0 && d == 0)
             break;
-        deg += ((40 - b + a) + (40 - b + c) + (40 - d + c)) * 9;
-        if (a == b)
-            deg -= (40 - b + a)*9;
-        if (b == c)
-            deg -= (40 - b + c)*9;
-        cout << deg << "\n";
+        if (a < b)
+            deg += a + 40 - b;
+        else
+            deg += a - b;
+        if (b > c)
+            deg += c + 40 - b;
+        else
+            deg += c - b;
+        if (c < d)
+            deg += c + 40 - d;
+        else
+            deg += c - d;
+        cout << deg*9 << "\n";
     }
 }
