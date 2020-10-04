9d50a389dfb21d0075e29e854a7d580d3f9d1063
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-10-04 13:29:37 +0600

    lab-02 p29 wrong answer 2 case

---
+++ lab-02/p29/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, deg;
+    while(true)
+    {
+        deg = 1080;
+        cin >> a >> b >> c >> d;
+        if(a == 0 && b ==0 && c == 0 && d == 0)break;
+        deg += ((40-b+a)+(40-b+c)+(40-d+c))*9;
+        cout << deg << "\n";
+    }
+}
