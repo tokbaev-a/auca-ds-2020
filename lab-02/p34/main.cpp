d998c06ab4c41fab29a6dfdf9d3daeba151462f0
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-10-14 03:15:41 +0600

    lab-02 p34 Accepted

--- lab-02/p34/main.cpp
+++ lab-02/p34/main.cpp
@@ -5,4 +5,26 @@ using namespace std;
 
 int main()
 {
+    int solved = 0;
+    int ans = 0;
+    int min = 0;
+    char b;
+    int c[25] = {0};
+    string a;
+    while(true)
+    {
+        cin >> min;
+        if(min == -1)break;
+        cin >> b >> a;
+        if(a == "wrong")
+        {
+        c[(static_cast<int>(b))-65]+=20;
+        }
+        else if (a == "right")
+        {
+            ans += (c[(static_cast<int>(b))-65] + min);
+            solved++;
+        }
+    }
+    cout << solved << " " << ans;
 }
