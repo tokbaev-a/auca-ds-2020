7061944eea6c361114f881a96c266ae0d89afec5
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-10-14 09:30:11 +0600

    lab-02 p36 Accepted

--- lab-02/p36/main.cpp
+++ lab-02/p36/main.cpp
@@ -6,43 +6,23 @@
 #include <vector>
 using namespace std;
 
-int index(int a[], int x, int n)
-{
-    int i = 0;
-    for (i = 0; i < n; i++)
-    {
-        if(a[i] == x)break;
-    }
-    return i;
-}
-
 int main()
 {
-    int n, min, ans1, ans2;
-    min = 1000;
+    int n, minmax, ans1;
+    minmax = 1000;
     cin >> n;
     int arr[n] = {0};
-    int arr1[n] = {0};
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
-        arr1[i] = arr[i];
     }
-    sort(arr1, arr1 + n);
-    for (int i = 0; i < n-2; i++)
+    for (int i = 0; i < n - 2; i++)
     {
-        if (index(arr1, arr[i], n) + index(arr1, arr[i + 2], n) < min)
+        if (max(arr[i], arr[i + 2]) < minmax)
         {
-            min = index(arr1, arr[i], n) + index(arr1, arr[i+2], n);
-            ans1 = i+1;
-            ans2 = max(arr[i], arr[i+2]);
-        }
-        else if (index(arr1, arr[i], n) + index(arr1, arr[i + 2], n) == min && max(arr[i], arr[i + 2]) < ans2)
-        {
-            min = index(arr1, arr[i], n) + index(arr1, arr[i + 2], n);
+            minmax = max(arr[i], arr[i + 2]);
             ans1 = i + 1;
-            ans2 = max(arr[i], arr[i + 2]);
         }
     }
-    cout << ans1 << " " << ans2 ;
+    cout << ans1 << " " << minmax;
 }
