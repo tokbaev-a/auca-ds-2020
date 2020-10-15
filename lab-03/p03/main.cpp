a81e66b09882eed1868dc539169ae6445cb60e45
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-10-15 23:00:29 +0600

    lab-03 p03 Accepted

--- lab-03/p03/main.cpp
+++ lab-03/p03/main.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 int main()
 {
-    int n, tmp;
+    int n, tmp, tmp1;
+    tmp1 = 0;
+    tmp = 0;
     string str;
     cin >> n;
     vector<string> hum;
@@ -13,14 +15,26 @@ int main()
         cin >> str;
         hum.push_back(str);
     }
-    for (int i = 0; i < song.size(); i++)
+    for (int i = 0; i < max(song.size(), hum.size()); i++)
     {
-        cout << hum.at(tmp) << ": " << song.at(i) << "\n";
+        if (i == max(song.size(), hum.size()) - 1 && tmp1 != song.size() - 1)
+            i--;
+        cout << hum.at(tmp) << ": " << song.at(tmp1) << "\n";
         if (tmp < hum.size() - 1)
         {
             tmp++;
         }
         else
+        {
             tmp = 0;
+        }
+        if (tmp1 < song.size() - 1)
+        {
+            tmp1++;
+        }
+        else
+        {
+            tmp1 = 0;
+        }
     }
 }
