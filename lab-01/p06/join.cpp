7aa3077753bc97e6136deb480e20f3747529c7c2
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-11-06 19:38:42 +0600

    lab01 p06 Working

---
+++ lab-01/p06/join.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string join(const vector<string> &v)
+{
+    string r;
+
+    if (!v.empty())
+    {
+        r = v[0];
+        for (size_t i = 1; i < v.size(); ++i)
+        {
+            r += " ";
+            r += v[i];
+        }
+    }
+
+    return r;
+}
