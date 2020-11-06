c5b594ae71e4659563b2a1024ffe72781ff96941
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-11-06 19:26:12 +0600

    lab01 p05 finished

---
+++ lab-01/p06/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<vector<string>> data = {
+        {"Hello", "World", "!!!"},
+        {"Hello", "World"},
+        {},
+        {"Hello", "World", "Again", "!!!"},
+    };
+
+    for (const auto &v : data)
+    {
+        cout << join(v) << "\n";
+    }
+}
