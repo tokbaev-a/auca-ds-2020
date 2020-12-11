b14ba7b444b7495b841c7132ae8ec72934b49653
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-12-11 23:28:56 +0600

    lab-04 p04 Done

---
+++ lab-04/p04/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10
+#define M 7
+
+void printArray(int *p, int n)
+{
+    for (int *q = p + n; p < q; ++p)
+    {
+        printf("%d ", *p);
+    }
+    printf("\n");
+}
+
+void reverse(int *beg, int *end)
+{
+    for (;;)
+    {
+        if (beg == end)
+        {
+            break;
+        }
+
+        --end;
+
+        if (beg == end)
+        {
+            break;
+        }
+
+        int t = *beg;
+        *beg = *end;
+        *end = t;
+
+        ++beg;
+    }
+}
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    // a - dynamic array
+    int *a = (int *)malloc(sizeof(int) * n);
+    if (a == NULL)
+    {
+        printf("Not enough dynamic memory\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d", &a[i]);
+    }
+    reverse(a, a + n);
+
+    printArray(a, n);
+
+    free(a);
+}
