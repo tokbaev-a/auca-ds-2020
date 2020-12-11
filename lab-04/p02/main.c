36c3196e2d4a92dfef4f4448cff0bd5f0347bbb4
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-12-11 23:29:16 +0600

    lab-04 p02 Fixed some bugs

--- lab-04/p02/main.c
+++ lab-04/p02/main.c
@@ -6,21 +6,35 @@ int main(void)
 {
     int a[N];
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; ++i)
     {
         scanf("%d", &a[i]);
     }
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; ++i)
     {
         printf("%d", a[i]);
     }
     printf("\n");
 
     // pointer arithmetic
-    for (int *p = &a[0]; p != a + N; p++)
+    for (int *p = &a[0]; p != a + N; ++p)
     {
         printf("%d", *p);
     }
     printf("\n");
+
+    int s1 = 0;
+    for(int i = 0; i < N; ++i)
+    {
+        s1 += a[i];
+    }
+    printf("Sum of all elements is %d\n", s1);
+
+    int s2 = 0;
+    for(int *p =a; p != &a[N]; ++p)
+    {
+        s2 += *p;
+    }
+    printf("Sum of all elements is %d\n", s2);
 }
