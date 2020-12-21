687c1418ef1507ac9e92ed759db74a9ca22f57e2
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-12-21 22:10:24 +0600

    lab-06 p11 Done

---
+++ lab-06/p11/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    vector<int> v = {0, 4, 5, 10, 12, 20, 25, 40};
+
+    while (cin >> x)
+    {
+        auto it = lower_bound(v.begin(), v.end(), x);
+
+        if (*it == x)
+        {
+            cout << "Index of first element equal to <" << x << "> is <" << it - v.begin() << ">\n";
+        }
+        else if (*it > x)
+        {
+            cout << "Index of first element greater than <" << x << "> is <" << it - v.begin() << ">\n";
+        }
+        else
+        {
+            cout << "No elements equal or greater than <" << x << ">\n";
+        }
+    }
+}
