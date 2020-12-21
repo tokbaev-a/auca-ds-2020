1c19af2966d438be00d99261877bc2f2df65273d
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-12-21 21:52:16 +0600

    lab-06 p10 Done

---
+++ lab-06/p10/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v = {0, 4, 5, 10, 12, 20, 25, 40};
+
+    int x;
+    while (cin >> x)
+    {
+        if (binary_search(v.begin(), v.end(), x))
+        {
+            cout << "Yes\n";
+        }
+        else
+        {
+            cout << "No\n";
+        }
+    }
+}
