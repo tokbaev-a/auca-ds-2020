6d90cb3be8c9239d5b5383ea19bbdaa7355b605d
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-12-21 23:57:22 +0600

    lab-07 p06 Done

---
+++ lab-07/p06/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <iterator>
+#include <algorithm>
+#include "../../libs/strutils/utils.hpp"
+
+using namespace std;
+
+struct Student
+{
+    string mName;
+    double mGpa;
+    Student(const string &name, double gpa)
+        : mName(name), mGpa(gpa)
+    {
+    }
+};
+
+int main()
+{
+    string name;
+    double gpa;
+    vector<Student> st_vec;
+
+    while (cin >> name >> gpa)
+    {
+        Student st(name, gpa);
+        st_vec.push_back(st);
+    }
+
+    cout << "By  GPA: ";
+    auSelectSort(st_vec.begin(), st_vec.end(), [](const Student &g1, const Student &g2) { return g1.mGpa < g2.mGpa; });
+
+    for (int i = 0; i < (int)st_vec.size(); i++)
+    {
+        cout << st_vec[i].mName << " " << st_vec[i].mGpa << ", ";
+    }
+    cout << "\n";
+
+    cout << "By NAME: ";
+    auSelectSort(st_vec.begin(), st_vec.end(), [](const Student &g1, const Student &g2) { return g1.mName < g2.mName; });
+    for (int i = 0; i < (int)st_vec.size(); i++)
+    {
+        cout << st_vec[i].mName << " " << st_vec[i].mGpa << ", ";
+    }
+    cout << "\n";
+}
