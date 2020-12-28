9977de13f8af6fdccb185024c5b7ffd1fee815c2
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-12-28 21:41:33 +0600

    project-01 kattis-	dyslectionary Time limit ex

---
+++ project-01/kattis-dyslectionary/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void config(vector<string> &words)
+{
+    sort(words.begin(), words.end(), [](const string &s1, const string &s2) {
+        return s1.length() > s2.length();
+    });
+
+    int max_length = words[0].length();
+
+    sort(words.begin(), words.end(), [](string s1, string s2) {
+        reverse(s1.begin(), s1.end());
+        reverse(s2.begin(), s2.end());
+        return s1 < s2;
+    });
+
+    for (auto &i : words)
+    {
+        cout << right << setw(max_length) << i << endl;
+    }
+}
+
+int main()
+{
+    string word;
+    vector<string> words;
+
+    while (true)
+    {
+        if (word.empty())
+        {
+            config(words);
+            cout << "\n";
+            words.clear();
+        }
+        words.push_back(word);
+    }
+    config(words);
+}
